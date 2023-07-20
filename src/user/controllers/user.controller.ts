baac471ea2ef16c824e9330d53d73f7095da27ca
Roberto Sousa <robertosousa186@gmail.com>
2023-07-19 22:51:19 -0300

    Creating user resource

---
+++ src/user/controllers/user.controller.ts
@@ -0,0 +1,52 @@
+import { Response, Request } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+
+const prisma = new PrismaClient();
+
+export class UserController {
+  async create(req: Request, res: Response) {
+    const data: CreateUserDto = req.body;
+
+    const user = await prisma.user.create({ data });
+
+    return res.status(200).json(user);
+  }
+
+  async findAll(req: Request, res: Response) {
+    const users = await prisma.user.findMany();
+
+    return res.json(users);
+  }
+
+  async findById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+
+    res.status(200).json(user);
+  }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const updateUserDto: UpdateUserDto = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: Number(id) },
+      data: updateUserDto,
+    });
+
+    res.status(200).json(updatedUser);
+  }
+
+  async remove(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const user = await prisma.user.delete({ where: { id: Number(id) } });
+
+    res.status(200).json(user);
+  }
+}
+
+// 305888394
---
+++ src/user/routes/user.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/user.controller';
+
+const router = Router();
+
+const userController = new UserController();
+
+router.post('/users', userController.create);
+router.get('/users', userController.findAll);
+router.get('/users/:id', userController.findById);
+router.patch('/users/:id', userController.update);
+router.delete('/users/:id', userController.remove);
+
+export default router;
